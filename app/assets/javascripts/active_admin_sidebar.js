c0c322e8b52a91285ad4cb84034e7c7a839d9dea
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-07 18:41:19 +0200

    Redesign toggle button as vertical pill, remove icono dependency
    - Replace icono-caret icons with slim vertical pill button (12x48px)
    - Chevron direction handled purely by CSS based on sidebar state
    - Simplify JS by removing icon class management
    - Remove active_admin_sidebar_pure_icons.scss (no longer needed)

--- app/assets/javascripts/active_admin_sidebar.js
+++ app/assets/javascripts/active_admin_sidebar.js
@@ -1,28 +1,13 @@
 (function() {
   $(document).ready(function() {
-    var $aa_content, set_collapsed_sidebar, isLeftSidebar, collapseIcon, expandIcon;
     if ($('body').hasClass('index') && ($('#active_admin_content').hasClass('collapsible_sidebar'))) {
-      $aa_content = $('#active_admin_content');
-      isLeftSidebar = $aa_content.hasClass('left_sidebar');
-      var isCollapsed = $aa_content.hasClass('collapsed_sidebar');
-
-      if (isLeftSidebar) {
-        collapseIcon = 'icono-caret-left';
-        expandIcon = 'icono-caret-right';
-      } else {
-        collapseIcon = 'icono-caret-right';
-        expandIcon = 'icono-caret-left';
-      }
-
-      var initialIcon = isCollapsed ? expandIcon : collapseIcon;
-      var $toggleBtn = $('<span class="sidebar_toggle_btn ' + initialIcon + '" title="Toggle sidebar"></span>');
-
-      // Place button inside #sidebar but outside .sidebar_section
-      // so it stays visible when sections are hidden
+      var $aa_content = $('#active_admin_content');
       var $sidebar = $aa_content.find('#sidebar');
+
+      var $toggleBtn = $('<span class="sidebar_toggle_btn" title="Toggle sidebar"><span class="chevron"></span></span>');
       $sidebar.prepend($toggleBtn);
 
-      set_collapsed_sidebar = function(value) {
+      var set_collapsed_sidebar = function(value) {
         return $.getJSON(this.href, {
           collapsed_sidebar: value
         });
@@ -32,16 +17,13 @@
         if (!$aa_content.hasClass('collapsed_sidebar')) {
           set_collapsed_sidebar(true);
           $aa_content.addClass('collapsed_sidebar');
-          $toggleBtn.removeClass(collapseIcon).addClass(expandIcon);
           $aa_content.trigger('collapsible_sidebar:collapsed');
         } else {
           set_collapsed_sidebar(false);
           $aa_content.removeClass('collapsed_sidebar');
-          $toggleBtn.removeClass(expandIcon).addClass(collapseIcon);
           $aa_content.trigger('collapsible_sidebar:uncollapsed');
         }
       });
     }
   });
-
 }).call(this);
